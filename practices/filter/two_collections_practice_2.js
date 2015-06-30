0935e27fddb8c0ab977132df63c8a05d05c964c6
jiaoming37 <13366648680@163.com>
2015-06-30 23:17:12 +0800

    a result sytle has a problem

--- practices/filter/two_collections_practice_2.js
+++ practices/filter/two_collections_practice_2.js
@@ -3,6 +3,27 @@
 function choose_no_common_elements(collection_a, collection_b) {
 
   //在这里写入代码
+  var result=[];
+
+  for(var i=0;i<collection_a.length;i++){
+
+    var not_common=true;
+
+    for(var j=0;j<collection_b.length;j++){
+
+      if(collection_a[i]==collection_b[j]){
+
+        not_common=false;
+
+        break;
+      }
+    }
+    if(not_common){
+
+      result.push(collection_a[i]);
+    }
+  }
+  return result;
 }
 
 module.exports = choose_no_common_elements;
--- practices/filter/two_collections_practice_3.js
+++ practices/filter/two_collections_practice_3.js
@@ -3,6 +3,27 @@
 function choose_divisible_integer(collection_a, collection_b) {
 
   //在这里写入代码
+  var result=[];
+
+  for(var i=0;i<collection_a.length;i++){
+
+    var can_be_divisible=false;
+
+    for(var j=0;j<collection_b.length;j++){
+
+      if(collection_a[i]%collection_b[j]==0){
+
+        can_be_divisible=true;
+
+        break;
+      }
+    }
+    if(can_be_divisible){
+
+      result.push(collection_a[i]);
+    }
+  }
+  return result;
 }
 
 module.exports = choose_divisible_integer;
